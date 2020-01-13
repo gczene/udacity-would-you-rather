d916e170437743d6f8a66732e6e9b7dda45f9c84
Gabor Czene <gabor.czene@britishgas.co.uk>
2020-01-13 17:35:39 +0000

    feat: Menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Menu from './components/Menu';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Router>
+      <div className="App">
+        <Menu />
+        app
+      </div>
+    </Router>
   );
 }
 
--- src/stories/1-Menu.stories.js
+++ src/stories/1-Menu.stories.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from '../components/Menu'
+import {BrowserRouter as Router} from "react-router-dom";
 
-export const DisplayMenu = () => <Menu />;
+export default {
+  title: 'Menu',
+  // component: Menu,
+}
 
-DisplayMenu.story = {
-  name: 'Menu',
+export const LoggedInMenu = () => {
+  const user = {
+    name: 'John Doe',
+    avatarURL: '/images/user-1.jpg'
+  };
+  return (
+    <Router>
+      <Menu user={user} />
+    </Router>
+    );
+};
+
+LoggedInMenu.story = {
+  name: 'Logged in',
   component: Menu
 };
 
+
+export const LoggedOutInMenu = () => {
+  return (
+    <Router>
+      <Menu />
+    </Router>
+  );
+};
+
+LoggedOutInMenu.story = {
+  name: 'Logged out',
+  component: Menu
+};
 
