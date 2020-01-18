38654f719cf45820dfdc0cda62770adab41eb8a9
Gabor Czene <gabor.czene@britishgas.co.uk>
2020-01-18 07:57:09 +0000

    refactor: removing console.log

--- src/reducers/votes.js
+++ src/reducers/votes.js
@@ -7,7 +7,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_VOTE:
-      console.log(action, state);
       return {
         ...state,
         byIds: {
